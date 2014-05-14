b81c2ea7e88ed9136e9ae241aeb10bcbb790312a
Eyal Susser <eyal.susser@gmail.com>
2014-05-13 21:06:29 -0400

    replace ring images and modify text for wedding: timeless bands; replace twitter image; move social icons to top right

--- includes/content_head.inc.php
+++ includes/content_head.inc.php
@@ -8,8 +8,26 @@
 			<div id="banner">
 				<h1>Anouk Jewelry <p class="tagline">Inspired by nature and the ancient world</p> </h1>
 				<div id="site-logo"><a href="http://anoukjewelry.com" title="home" alt="home" ></a></div>
+
+				<div id="wrap-social-plugin">
+
+					<div id="facebook">
+						<a href="http://www.facebook.com/pages/Anouk-Jewelry/113533041996730" target="_blank"><img src="<?php echo $path ?>imgs/facebook.png" class="social-link"></a>
+					</div>
+					
+					<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script> 
+					<div id="tweeter"> 
+						<a href="https://twitter.com/anoukbyanat" target="_blank"><img src="<?php echo $path ?>imgs/twitter.png" class="social-link""></a> 
+					</div>
+					
+					<div>
+						<a href="http://www.pinterest.com/anoukbyanat" target="_blank"><img src="<?php echo $path ?>imgs/pinterest.png" class="social-link""></a> 
+					</div>
+
+				</div>	
 			</div>
 			<!--End: id="banner" -->
+
 
 
 			<!--Begin: id="nav-main" -->
